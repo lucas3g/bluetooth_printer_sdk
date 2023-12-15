6988001ca8e53e61bf3332b0ae7b148fe07cdc23
Lucas Silva <lucassilva080397@gmail.com>
2023-12-15 17:30:03 -0300

    feat: many improves

--- BluetoothPrinterELLib/src/main/java/com/elsistemas/bluetoothprinterellib/BlueThermalPrinterPlugin.java
+++ BluetoothPrinterELLib/src/main/java/com/elsistemas/bluetoothprinterellib/BlueThermalPrinterPlugin.java
@@ -1,16 +1,17 @@
 package com.elsistemas.bluetoothprinterellib;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
-
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,11 +30,10 @@ import java.util.UUID;
 
 public class BlueThermalPrinterPlugin {
     private final Context context;
-
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static ConnectedThread THREAD = null;
     private BluetoothAdapter mBluetoothAdapter;
-
+    private static final int REQUEST_ENABLE_BT = 1;
     private final Object initializationLock = new Object();
     private BluetoothManager mBluetoothManager;
 
@@ -44,7 +44,7 @@ public class BlueThermalPrinterPlugin {
         }
     }
 
-    public BlueThermalPrinterPlugin(Context context){
+    public BlueThermalPrinterPlugin(Context context) {
         this.context = context;
 
         setup();
@@ -87,6 +87,7 @@ public class BlueThermalPrinterPlugin {
         // Retornar a representação em string do JSON
         return jsonArray.toString();
     }
+
     @SuppressLint("MissingPermission")
     public String getPairedDevices() throws JSONException {
 
@@ -111,7 +112,7 @@ public class BlueThermalPrinterPlugin {
     }
 
     @SuppressLint("MissingPermission")
-    public boolean connect(String address) {
+    public boolean connect(String address, int widthPaper) {
         try {
             if (THREAD != null) {
                 // Já conectado
@@ -139,6 +140,11 @@ public class BlueThermalPrinterPlugin {
                 socket.connect();
                 THREAD = new ConnectedThread(socket);
                 THREAD.start();
+
+                byte[] command = {0x1B, 0x40, 0x1B, 0x57, (byte)(widthPaper / 8), 0x0A}; // set width paper
+
+                THREAD.write(command);
+
                 return true;  // Conexão estabelecida com sucesso
             } catch (Exception ex) {
                 // Erro durante a conexão
@@ -162,25 +168,25 @@ public class BlueThermalPrinterPlugin {
             });
 
             return true;
-        } catch (Exception ex){
+        } catch (Exception ex) {
             return false;
         }
     }
 
-    public boolean write(String message) throws Exception {
+    private boolean write(String message) throws Exception {
         if (THREAD == null) {
             throw new Exception("not connected");
         }
 
         try {
             THREAD.write(message.getBytes());
-           return true;
+            return true;
         } catch (Exception ex) {
             throw new Exception(ex.getMessage());
         }
     }
 
-    public boolean writeBytes(byte[] message) throws Exception {
+    private boolean writeBytes(byte[] message) throws Exception {
         if (THREAD == null) {
             throw new Exception("not connected");
         }
@@ -193,15 +199,15 @@ public class BlueThermalPrinterPlugin {
         }
     }
 
-    public boolean printCustom(String message, int size, int align) throws Exception {
+    public boolean printText(String message, int size, int align) throws Exception {
         // Print config "mode"
-        byte[] cc = new byte[] { 0x1B, 0x21, 0x03 }; // 0- normal size text
+        byte[] cc = new byte[]{0x1B, 0x21, 0x03}; // 0- normal size text
         // byte[] cc1 = new byte[]{0x1B,0x21,0x00}; // 0- normal size text
-        byte[] bb = new byte[] { 0x1B, 0x21, 0x08 }; // 1- only bold text
-        byte[] bb2 = new byte[] { 0x1B, 0x21, 0x20 }; // 2- bold with medium text
-        byte[] bb3 = new byte[] { 0x1B, 0x21, 0x10 }; // 3- bold with large text
-        byte[] bb4 = new byte[] { 0x1B, 0x21, 0x30 }; // 4- strong text
-        byte[] bb5 = new byte[] { 0x1B, 0x21, 0x50 }; // 5- extra strong text
+        byte[] bb = new byte[]{0x1B, 0x21, 0x08}; // 1- only bold text
+        byte[] bb2 = new byte[]{0x1B, 0x21, 0x20}; // 2- bold with medium text
+        byte[] bb3 = new byte[]{0x1B, 0x21, 0x10}; // 3- bold with large text
+        byte[] bb4 = new byte[]{0x1B, 0x21, 0x30}; // 4- strong text
+        byte[] bb5 = new byte[]{0x1B, 0x21, 0x50}; // 5- extra strong text
         if (THREAD == null) {
             throw new Exception("not connected");
         }
@@ -248,55 +254,57 @@ public class BlueThermalPrinterPlugin {
             THREAD.write(PrinterCommands.FEED_LINE);
             THREAD.write(PrinterCommands.FEED_LINE);
             THREAD.write(PrinterCommands.FEED_LINE);
+            THREAD.write(PrinterCommands.FEED_LINE);
+            THREAD.write(PrinterCommands.FEED_LINE);
             return true;
         } catch (Exception ex) {
             throw new Exception(ex.getMessage());
         }
     }
 
-    public boolean printLeftRight(String msg1, String msg2, int size ,String charset,String format) throws Exception {
-        byte[] cc = new byte[] { 0x1B, 0x21, 0x03 }; // 0- normal size text
-        // byte[] cc1 = new byte[]{0x1B,0x21,0x00}; // 0- normal size text
-        byte[] bb = new byte[] { 0x1B, 0x21, 0x08 }; // 1- only bold text
-        byte[] bb2 = new byte[] { 0x1B, 0x21, 0x20 }; // 2- bold with medium text
-        byte[] bb3 = new byte[] { 0x1B, 0x21, 0x10 }; // 3- bold with large text
-        byte[] bb4 = new byte[] { 0x1B, 0x21, 0x30 }; // 4- strong text
-        if (THREAD == null) {
-            throw new Exception("not connected");
-        }
-        try {
-            switch (size) {
-                case 0:
-                    THREAD.write(cc);
-                    break;
-                case 1:
-                    THREAD.write(bb);
-                    break;
-                case 2:
-                    THREAD.write(bb2);
-                    break;
-                case 3:
-                    THREAD.write(bb3);
-                    break;
-                case 4:
-                    THREAD.write(bb4);
-                    break;
-            }
-            THREAD.write(PrinterCommands.ESC_ALIGN_CENTER);
-            String line = String.format("%-15s %15s %n", msg1, msg2);
-            if(format != null) {
-                line = String.format(format, msg1, msg2);
-            }
-            if(charset != null) {
-                THREAD.write(line.getBytes(charset));
-            } else {
-                THREAD.write(line.getBytes());
-            }
-            return true;
-        } catch (Exception ex) {
-            throw new Exception(ex.getMessage());
-        }
-    }
+//    public boolean printLeftRight(String msg1, String msg2, int size, String charset, String format) throws Exception {
+//        byte[] cc = new byte[]{0x1B, 0x21, 0x03}; // 0- normal size text
+//        // byte[] cc1 = new byte[]{0x1B,0x21,0x00}; // 0- normal size text
+//        byte[] bb = new byte[]{0x1B, 0x21, 0x08}; // 1- only bold text
+//        byte[] bb2 = new byte[]{0x1B, 0x21, 0x20}; // 2- bold with medium text
+//        byte[] bb3 = new byte[]{0x1B, 0x21, 0x10}; // 3- bold with large text
+//        byte[] bb4 = new byte[]{0x1B, 0x21, 0x30}; // 4- strong text
+//        if (THREAD == null) {
+//            throw new Exception("not connected");
+//        }
+//        try {
+//            switch (size) {
+//                case 0:
+//                    THREAD.write(cc);
+//                    break;
+//                case 1:
+//                    THREAD.write(bb);
+//                    break;
+//                case 2:
+//                    THREAD.write(bb2);
+//                    break;
+//                case 3:
+//                    THREAD.write(bb3);
+//                    break;
+//                case 4:
+//                    THREAD.write(bb4);
+//                    break;
+//            }
+//            THREAD.write(PrinterCommands.ESC_ALIGN_CENTER);
+//            String line = String.format("%-15s %15s %n", msg1, msg2);
+//            if (format != null) {
+//                line = String.format(format, msg1, msg2);
+//            }
+//            if (charset != null) {
+//                THREAD.write(line.getBytes(charset));
+//            } else {
+//                THREAD.write(line.getBytes());
+//            }
+//            return true;
+//        } catch (Exception ex) {
+//            throw new Exception(ex.getMessage());
+//        }
+//    }
 
     public boolean printNewLine(int count) throws Exception {
         if (THREAD == null) {
@@ -304,7 +312,7 @@ public class BlueThermalPrinterPlugin {
         }
 
         try {
-            if (count > 1){
+            if (count > 1) {
                 for (int i = 0; i < count; i++) {
                     THREAD.write(PrinterCommands.FEED_LINE);
                 }
@@ -318,7 +326,7 @@ public class BlueThermalPrinterPlugin {
         }
     }
 
-    public class ConnectedThread extends Thread {
+    private class ConnectedThread extends Thread {
         private final BluetoothSocket mmSocket;
         private final InputStream inputStream;
         private final OutputStream outputStream;
@@ -375,45 +383,80 @@ public class BlueThermalPrinterPlugin {
         }
     }
 
-    public boolean printImage(String pathImage) throws Exception  {
+    public boolean printImage(Bitmap image) throws Exception {
         if (THREAD == null) {
-            throw  new Exception("not connected");
+            throw new Exception("not connected");
         }
 
         try {
-            Bitmap bmp = BitmapFactory.decodeFile(pathImage);
-            if (bmp != null) {
-                byte[] command = Utils.decodeBitmap(bmp);
+            if (image != null) {
+                byte[] command = Utils.decodeBitmap(image);
                 THREAD.write(PrinterCommands.ESC_ALIGN_CENTER);
                 THREAD.write(command);
             }
 
             return true;
         } catch (Exception ex) {
-            throw  new Exception(ex.getMessage());
+            throw new Exception(ex.getMessage());
         }
     }
 
-    public boolean printImageBytes(byte[] bytes) throws Exception {
+    public boolean printImageBytes(byte[] bytes, int align) throws Exception {
         if (THREAD == null) {
-            throw  new Exception("not connected");
+            throw new Exception("not connected");
         }
         try {
             Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
             if (bmp != null) {
                 byte[] command = Utils.decodeBitmap(bmp);
-                byte[] paperWidth = new byte[] {0x1D, 0x67, 0x50, 0x38, 0x30};
+                byte[] paperWidth = new byte[]{0x1D, 0x67, 0x50, 0x38, 0x30};
 
-                //THREAD.write(paperWidth);
-                THREAD.write(PrinterCommands.ESC_ALIGN_CENTER);
+                switch (align) {
+                    case 0:
+                        THREAD.write(PrinterCommands.ESC_ALIGN_LEFT);
+                        break;
+                    case 1:
+                        THREAD.write(PrinterCommands.ESC_ALIGN_CENTER);
+                        break;
+                    case 2:
+                        THREAD.write(PrinterCommands.ESC_ALIGN_RIGHT);
+                        break;
+                }
+
                 THREAD.write(command);
+
+                THREAD.write(PrinterCommands.FEED_LINE);
+                THREAD.write(PrinterCommands.FEED_LINE);
+                THREAD.write(PrinterCommands.FEED_LINE);
+                THREAD.write(PrinterCommands.FEED_LINE);
+                THREAD.write(PrinterCommands.FEED_LINE);
+                THREAD.write(PrinterCommands.FEED_LINE);
+                THREAD.write(PrinterCommands.FEED_LINE);
+                return true;
+
             } else {
                 Log.e("Print Photo error", "the file isn't exists");
             }
-            return true;
+            return false;
         } catch (Exception ex) {
             Log.e("Print image", ex.getMessage(), ex);
-             throw  new Exception("not connected");
+            throw new Exception("not connected");
+        }
+    }
+
+    public boolean bluetoothActivated() {
+        return mBluetoothAdapter.isEnabled();
+    }
+
+    @SuppressLint("MissingPermission")
+    public boolean enableBluetooth(Activity activity) {
+        try {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 }
